ffd4825dbb4f16ee8dba06a75aaacdeff75f769a
Henry Fernando <79999547+henry-marcondes@users.noreply.github.com>
2025-09-23 22:39:58 +0000

    Criando conteiner codespace, e hello.java

---
+++ Aula_Java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    // Duas barra indica um comentário de uma linha
+   
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+ /* Barra e asterisco indicam um comentário de múltiplas linhas
+       Este é um exemplo de comentário */
+
+    }
+}   
