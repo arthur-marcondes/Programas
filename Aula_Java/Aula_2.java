bc09e960b982453026063d6f258cab0b62f03f25
Henry Fernando <79999547+henry-marcondes@users.noreply.github.com>
2025-09-24 23:32:31 +0000

    coreção ortografica

--- Aula_Java/Aula_2.java
+++ Aula_Java/Aula_2.java
@@ -5,7 +5,7 @@ public class Aula_2 {
     // sabendo que por dia é descarregado 8 caminhões 
     // e cada caminhão tem 112 caixas
     // e cada caixa tem 74 unidades de produto.
-    // uqantas caixas serão armazenadas por mês (30 dias)?
+    // quantas caixas serão armazenadas por mês (30 dias)?
     
     
 }
